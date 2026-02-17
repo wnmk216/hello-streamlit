09bbb4c8d3f1733bbf2e1a617f03a75d987b90b9
wnmk216 <wanthanee.p@itm.kmutnb.ac.th>
2026-02-17 10:08:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+#Import Library 
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+#Display Title
+st.title("Hello Machine Learning!!!")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+
+#Display Header and SubHeader
+st.header("This is a header")
+st.header("This is a header with a divider", divider="gray")
+st.header("These headers have rotating dividers", divider=True)
+st.subheader("This is a subheader")
+st.subheader("One", divider=True)
+st.subheader("Two", divider=True)
